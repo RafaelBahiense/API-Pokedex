f8a62fadddb352641c70401e57cb800348aedae9
ronald <ronaldjunior2@hotmail.com>
2021-08-13 18:10:36 -0300

    Preparando revisão de código

--- tests/seederDB/functions/getPokeDescription.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default async function getPokeDescription(APIURL: string) {
-  const request = await axios.get(APIURL);
-
-  const { description } = request.data.descriptions[2];
-
-  return description;
-}
